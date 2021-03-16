ce60a8a275e34cec7423d2ae549d7afcc8ab39a4
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2021-03-16 00:08:19 -0300

    add validations for sale item

--- src/models/sale_item.py
+++ src/models/sale_item.py
@@ -1,14 +1,22 @@
 from bson import ObjectId
 import datetime
 
-from mongoengine import Document, StringField, DateTimeField, DecimalField
+from mongoengine import Document, StringField, DateTimeField, DecimalField, ValidationError, \
+    ObjectIdField
+
+ALLOWED_SELLERS = ['foo', 'bar', 'a', 'b', 'c']
+
+
+def valid_seller_name(seller_name):
+    if seller_name.lower() not in ALLOWED_SELLERS:
+        raise ValidationError(field_name='seller_name',
+                              message="{} is not an allowed seller.".format(seller_name))
 
 
 class SaleItem(Document):
-    _id = StringField(primary_key=True, default=ObjectId)
-    seller_name = StringField(max_length=300, required=True)
+    _id = ObjectIdField(primary_key=True, default=ObjectId)
+    seller_name = StringField(max_length=300, required=True, validation=valid_seller_name)
     customer_name = StringField(max_length=300, required=True)
     item_name = StringField(max_length=300, required=True)
-    item_value = DecimalField(min_value=0)
+    item_value = DecimalField(min_value=0, required=True)
     sale_date = DateTimeField(default=datetime.datetime.utcnow)
-
